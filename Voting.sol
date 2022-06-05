e5d8de3d40f12717f96cac67b4702e792baa2a6a
Naman Garg <97289118+naman-ng@users.noreply.github.com>
2022-06-05 16:38:16 +0530

    Add Functions

--- Voting.sol
+++ Voting.sol
@@ -2,7 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract Voting {
-    constructor() {}
+    constructor(string memory _ballotOfficialName, string memory _proposal) {
+        ballotOfficialAddress = msg.sender;
+        ballotOfficialName = _ballotOfficialName;
+        proposol = _proposal;
+
+        state = State.Created;
+    }
 
     struct vote {
         address voterAddress;
@@ -32,4 +38,56 @@ contract Voting {
         Ended
     }
     State public state;
+    
+    modifier condition(bool _condition) {
+        require(_condition);
+        _;
+    }
+
+    modifier onlyOfficial() {
+        require(msg.sender == ballotOfficialAddress);
+        _;
+    }
+
+    modifier inState(State _state) {
+        require(state == _state);
+        _;
+    }
+
+    function addVoter(address _voterAddress, string memory _voterName)
+        public
+        inState(State.created)
+        onlyOfficial
+    {
+        voter memory v;
+        v.voterName = _voterName;
+        v.voted = false;
+        voterRegister[_voterAddress] = v;
+        totalVoter ++ ;
+    }
+
+    function startVote() public inState(State.Created) onlyOfficial  {
+        state = State.Voting;
+    }
+
+    function doVote(bool _choice) public inState(State.Voting) returns (bool voted) {
+        bool found = false;
+
+        if (voterRegister[msg.sender].voterName.length !=0 && !voterRegister[msg.sender].voted) {
+            voterRegister[msg.sender].voted = true;
+            vote memory v;
+            v.voterAddress = msg.sender;
+            v.choice = _choice;
+            if (_choice) {
+                countResult++;
+            }            
+            vote[totalVotes] = v;
+            totalVotes++;
+            found = true;
+        }
+        return found;
+    }
+
+    function endVote(){}
+
 }
